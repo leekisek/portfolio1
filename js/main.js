e901a8cf21d767cf57c8471c631de277165a2388
leekisek <122263153+leekisek@users.noreply.github.com>
2023-02-08 18:24:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,80 @@
-var ww = $(window).width()
-if (ww > 1024) {
-    $('html').addClass('pc')
-} else {
-    $('html').addClass('mobile')
+
+
+
+
+var deviceSize = 1024
+function scrollOX(status) {
+    $('html').css({
+
+        overflowY:status
+    })    
+
+    return $('html').width()
 }
+
+// 토글디바이스툴바가 꺼져 있으면 스크롤바 유무에 따라 html의 넓이가 다르므로
+// 스크롤바 넓이 17px을 deviceSize에서 빼야 함
+
+var scX = scrollOX('hidden')
+var scO =  scrollOX('scroll')
+var scD = scX - scO
+
+// 토글 디바이스툴바가 켜져 있으면 scX와 scO는 같은 값이 되므로
+// 아래 if 문을 들어가지 않아서 diviceSize는 원래 값임.
+
+if(scD>0) {
+
+    deviceSize = deviceSize -scD
+    
+} 
+
+// pc화면용 네비게이션 액션
+
+$('#header #nav .depth1 > li').on('mouseover mouseout', function () {
+    if ($('html').hasClass('pc')) {
+        $(this).find('.depth2').stop().slideToggle()
+    }
+})
+
+function init() {
+
+    $(window).on('resize', function () {
+        let ww = $(this).width()
+        if (ww > deviceSize) {
+            $('html').addClass('pc')
+        } else {
+            $('html').addClass('mobile')
+        }
+    })
+
+}
+
+init()
+
+$(window).on('resize', function () {
+    let ww = $(this).width()
+    if (ww > deviceSize) {
+        $('html').addClass('pc').removeClass('mobile')
+    } else {
+        $('html').addClass('mobile').removeClass('pc')
+    }
+})
+
+$(window).on('resize',function(){
+    init()
+})
+
+
+
+
+
+
+// var ww = $(window).width()
+// if (ww > 1024) {
+//     $('html').addClass('pc')
+// } else {
+//     $('html').addClass('mobile')
+// }
 
 $('.header .topbar_bg .topbar .depth1>li').on('mouseover mouseout', function(){
     if ( $('html').hasClass('pc') ) {
@@ -69,16 +140,13 @@ $('.article3 .roomtxt .txt_group').slick({
 
 $('.article3 .imgbox2').slick({
     autoplay : true,
-    dots: true, 
+    dots: false, 
     arrows: false,
     pasueOnHover: true
 })
 
-$('.article3 .slick-dots li').on('click',function(){
-    if ('.article3 .slick-dots li:nth-child(1)') {
-        $('.article3 .imgbox2').next('roomimg1 rimg').show()
-    } else {
-        $('.article3 .imgbox2').children().hide()
-    }
-})
+// $('.article3 .slick-dots li').on('click',function(){
+//     if ('.article3 .slick-dots li:nth-child(1)') {
+//         $('.article3 .imgbox2').next('roomimg1 rimg').show()
+//     }} )
 
